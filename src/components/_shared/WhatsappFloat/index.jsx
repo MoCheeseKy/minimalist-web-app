3ec28754f4209ee7931c416400ee99c6580a04d0
Your NameMoCheeseKy <rifkyking04@gmail.com>
2024-11-16 00:16:32 +0700

    .

--- src/components/_shared/WhatsappFloat/index.jsx
+++ src/components/_shared/WhatsappFloat/index.jsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from 'react-icons/fa';
 export default function WhatsappFloat() {
   return (
     <WrapperComponent className='fixed z-[999] bottom-16 md:bottom-24 bg-transparent'>
-      <div className='w-full flex justify-end'>
+      <div className='w-full flex justify-end bg-transparent'>
         <a target='_blank' href='https://wa.me/+6281210714320'>
           <div className='w-12 h-12 bg-[#41c25d] rounded-full flex justify-center items-center text-3xl text-white'>
             <FaWhatsapp />
